e554223e12a27b9e6346820afe2e61112da12095
Olov Lassus <olov.lassus@gmail.com>
2014-05-21 13:23:17 +0200

    accept input via stdin if - is provided instead of filename

--- ng-annotate.js
+++ ng-annotate.js
@@ -12,6 +12,7 @@ const ngAnnotate = require("./ng-annotate-main");
 const version = require("./package.json").version;
 const optimist = require("optimist")
     .usage("ng-annotate v" + version + "\n\nUsage: ng-annotate OPTIONS <file>\n\n" +
+        "provide - instead of <file> to read from stdin\n" +
         "use -a and -r together to remove and add (rebuild) annotations in one go")
     .options("a", {
         alias: "add",
@@ -51,86 +52,112 @@ function exit(msg) {
     process.exit(-1);
 }
 
-(function verifyOptions() {
-    if (argv._.length !== 1) {
-        optimist.showHelp();
-        exit("error: no input file provided");
-    }
+// validate options
+if (argv._.length !== 1) {
+    optimist.showHelp();
+    exit("error: no input file provided");
+}
 
-    if (!argv.add && !argv.remove) {
-        optimist.showHelp();
-        exit("error: missing option --add and/or --remove");
-    }
-})();
+if (!argv.add && !argv.remove) {
+    optimist.showHelp();
+    exit("error: missing option --add and/or --remove");
+}
 
 const filename = argv._.shift();
 
-if (!fs.existsSync(filename)) {
-    exit(fmt('error: file not found {0}', filename));
-}
+(filename === "-" ? slurpStdin : slurpFile)(runAnnotate);
 
-const src = String(fs.readFileSync(filename));
 
-const config = tryor(function() {
-    return JSON.parse(String(fs.readFileSync("ng-annotate-config.json")));
-}, {});
+function slurpStdin(cb) {
+    let buf = "";
 
-function addOption(opt) {
-    if (opt in argv) {
-        config[opt] = argv[opt];
-    }
-}
-
-["add", "remove", "o", "regexp", "single_quotes", "plugin", "stats"].forEach(addOption);
-
-if (config.plugin) {
-    if (!Array.isArray(config.plugin)) {
-        config.plugin = [config.plugin];
-    }
-    config.plugin = config.plugin.map(function(path) {
-        const absPath = tryor(fs.realpathSync.bind(fs, path), null);
-        if (!absPath) {
-            exit(fmt('error: plugin file not found {0}', path));
-        }
-        // the require below may throw an exception on parse-error
-        // that is fine because it gives the user the line info
-        return require(absPath);
+    process.stdin.setEncoding("utf8");
+    process.stdin.on("data", function(d) {
+        buf += d;
     });
+    process.stdin.on("end", function() {
+        cb(null, buf);
+    });
+    process.stdin.resume();
 }
 
-const run_t0 = Date.now();
-const ret = ngAnnotate(src, config);
-const run_t1 = Date.now();
-
-if (ret.errors) {
-    process.stderr.write(ret.errors.join("\n") + "\n");
-    process.exit(1);
-}
-
-const stats = ret._stats;
-if (config.stats && stats) {
-    const t1 = Date.now();
-    const all = t1 - t0;
-    const run_esprima = stats.esprima_parse_t1 - stats.esprima_parse_t0;
-    const all_esprima = run_esprima + (stats.esprima_require_t1 - stats.esprima_require_t0);
-    const nga_run = (run_t1 - run_t0) - run_esprima;
-    const nga_init = all - all_esprima - nga_run;
-
-    const pct = function(n) {
-        return Math.round(100 * n / all);
+function slurpFile(cb) {
+    if (!fs.existsSync(filename)) {
+        cb(new Error(fmt('error: file not found {0}', filename)));
     }
 
-    process.stderr.write(fmt("[{0} ms] esprima: {1}, nga init: {2}, nga run: {3}\n", all, all_esprima, nga_init, nga_run));
-    process.stderr.write(fmt("[%] esprima: {0}, nga init: {1}, nga run: {2}\n", pct(all_esprima), pct(nga_init), pct(nga_run)));
+    fs.readFile(filename, cb);
 }
 
-if (ret.src && config.o) {
-    try {
-        fs.writeFileSync(config.o, ret.src);
-    } catch (e) {
-        process.stderr.write(e.message + "\n");
-        process.exit(1);
+function runAnnotate(err, src) {
+    if (err) {
+        exit(err.message);
     }
-} else if (ret.src) {
-    process.stdout.write(ret.src);
+
+    src = String(src);
+
+    const config = tryor(function() {
+        return JSON.parse(String(fs.readFileSync("ng-annotate-config.json")));
+    }, {});
+
+
+    ["add", "remove", "o", "regexp", "single_quotes", "plugin", "stats"].forEach(function(opt) {
+        if (opt in argv) {
+            config[opt] = argv[opt];
+        }
+    });
+
+    if (config.plugin) {
+        if (!Array.isArray(config.plugin)) {
+            config.plugin = [config.plugin];
+        }
+        config.plugin = config.plugin.map(function(path) {
+            const absPath = tryor(fs.realpathSync.bind(fs, path), null);
+            if (!absPath) {
+                exit(fmt('error: plugin file not found {0}', path));
+            }
+            // the require below may throw an exception on parse-error
+            try {
+                require(absPath)
+            } catch (e) {
+                // node will already print file:line and offending line to stderr
+                exit(fmt("error: couldn't require(\"{0}\")", absPath));
+            }
+        });
+    }
+
+    const run_t0 = Date.now();
+    const ret = ngAnnotate(src, config);
+    const run_t1 = Date.now();
+
+    if (ret.errors) {
+        exit(ret.errors.join("\n"));
+    }
+
+    const stats = ret._stats;
+    if (config.stats && stats) {
+        const t1 = Date.now();
+        const all = t1 - t0;
+        const run_esprima = stats.esprima_parse_t1 - stats.esprima_parse_t0;
+        const all_esprima = run_esprima + (stats.esprima_require_t1 - stats.esprima_require_t0);
+        const nga_run = (run_t1 - run_t0) - run_esprima;
+        const nga_init = all - all_esprima - nga_run;
+
+        const pct = function(n) {
+            return Math.round(100 * n / all);
+        }
+
+        process.stderr.write(fmt("[{0} ms] esprima: {1}, nga init: {2}, nga run: {3}\n", all, all_esprima, nga_init, nga_run));
+        process.stderr.write(fmt("[%] esprima: {0}, nga init: {1}, nga run: {2}\n", pct(all_esprima), pct(nga_init), pct(nga_run)));
+    }
+
+    if (ret.src && config.o) {
+        try {
+            fs.writeFileSync(config.o, ret.src);
+        } catch (e) {
+            exit(e.message);
+        }
+    } else if (ret.src) {
+        process.stdout.write(ret.src);
+    }
 }
