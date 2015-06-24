35261777fad14a421e26310d5e7cf2283c7d7df7
Olov Lassus <olov.lassus@gmail.com>
2015-06-24 20:47:18 +0200

    prepare.sh: .gitignore shouldn't be included in npm package. closes #178

--- build/prepare.sh
+++ build/prepare.sh
@@ -9,6 +9,8 @@ cd ng-annotate/build
 ./build.sh
 # delete build scripts
 rm *.sh *.js defs-config.json ng-annotate
+# delete .gitignore
+rm ../.gitignore
 # delete large test artifacts
 rm ../tests/angular.js ../build/es5/tests/angular.js
 cd ../..
